5d8cc5066803cde3b43d4488176d99c581b9a746
Jaime Pereira <jaime@iguana-farm.com>
2019-04-08 08:31:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p>Hello there Jaime and Friends!
           </p>
           <a
             className="App-link"
