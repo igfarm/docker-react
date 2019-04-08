777b5443f193242fd782860026f8fb31103807b0
Jaime Pereira <jaime@iguana-farm.com>
2019-04-08 16:29:13 -0400

    some random change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello there Jaime and Friends!
+          <p>This is a change on feature
           </p>
           <a
             className="App-link"
